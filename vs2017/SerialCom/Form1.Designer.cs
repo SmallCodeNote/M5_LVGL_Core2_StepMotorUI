a65f90778d9d8a3e78e2220b2b57f967206fb9b0
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2025-10-28 05:22:48 +0900

    Add files via upload

---
+++ vs2017/SerialCom/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace FTDI_AccessCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_PortName = new System.Windows.Forms.ComboBox();
+            this.comboBox_BoadRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_DataBit = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_StopBit = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox_Parity = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "port";
+            // 
+            // comboBox_PortName
+            // 
+            this.comboBox_PortName.FormattingEnabled = true;
+            this.comboBox_PortName.Location = new System.Drawing.Point(62, 12);
+            this.comboBox_PortName.Name = "comboBox_PortName";
+            this.comboBox_PortName.Size = new System.Drawing.Size(62, 20);
+            this.comboBox_PortName.TabIndex = 1;
+            // 
+            // comboBox_BoadRate
+            // 
+            this.comboBox_BoadRate.FormattingEnabled = true;
+            this.comboBox_BoadRate.Items.AddRange(new object[] {
+            " 300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "128000",
+            "256000"});
+            this.comboBox_BoadRate.Location = new System.Drawing.Point(62, 43);
+            this.comboBox_BoadRate.Name = "comboBox_BoadRate";
+            this.comboBox_BoadRate.Size = new System.Drawing.Size(62, 20);
+            this.comboBox_BoadRate.TabIndex = 2;
+            this.comboBox_BoadRate.Text = "115200";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "boadrate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "databit";
+            // 
+            // comboBox_DataBit
+            // 
+            this.comboBox_DataBit.FormattingEnabled = true;
+            this.comboBox_DataBit.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox_DataBit.Location = new System.Drawing.Point(62, 78);
+            this.comboBox_DataBit.Name = "comboBox_DataBit";
+            this.comboBox_DataBit.Size = new System.Drawing.Size(62, 20);
+            this.comboBox_DataBit.TabIndex = 2;
+            this.comboBox_DataBit.Text = "8";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "stopbit";
+            // 
+            // comboBox_StopBit
+            // 
+            this.comboBox_StopBit.FormattingEnabled = true;
+            this.comboBox_StopBit.Items.AddRange(new object[] {
+            "None",
+            "One",
+            "Two",
+            "OnePointFive"});
+            this.comboBox_StopBit.Location = new System.Drawing.Point(62, 114);
+            this.comboBox_StopBit.Name = "comboBox_StopBit";
+            this.comboBox_StopBit.Size = new System.Drawing.Size(62, 20);
+            this.comboBox_StopBit.TabIndex = 4;
+            this.comboBox_StopBit.Text = "1";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(9, 190);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(115, 37);
+            this.button_Connect.TabIndex = 5;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "parity";
+            // 
+            // comboBox_Parity
+            // 
+            this.comboBox_Parity.FormattingEnabled = true;
+            this.comboBox_Parity.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even",
+            "Mark",
+            "Space"});
+            this.comboBox_Parity.Location = new System.Drawing.Point(62, 154);
+            this.comboBox_Parity.Name = "comboBox_Parity";
+            this.comboBox_Parity.Size = new System.Drawing.Size(62, 20);
+            this.comboBox_Parity.TabIndex = 7;
+            this.comboBox_Parity.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 538);
+            this.Controls.Add(this.comboBox_Parity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.comboBox_StopBit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox_DataBit);
+            this.Controls.Add(this.comboBox_BoadRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox_PortName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FTDI_AccessCode";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_PortName;
+        private System.Windows.Forms.ComboBox comboBox_BoadRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_DataBit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_StopBit;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox_Parity;
+    }
+}
+
---
+++ vs2017/SerialCom/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using WinFormStringCnvClass;
+
+namespace FTDI_AccessCode
+{
+    public partial class Form1 : Form
+    {
+        string thisExeDirPath;
+        public Form1()
+        {
+            InitializeComponent();
+            thisExeDirPath = Path.GetDirectoryName(Application.ExecutablePath);
+            initPortNameItems();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TEXT|*.txt";
+            if (false && ofd.ShowDialog() == DialogResult.OK)
+            {
+                WinFormStringCnv.setControlFromString(this, File.ReadAllText(ofd.FileName));
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                if (File.Exists(paramFilename))
+                {
+                    WinFormStringCnv.setControlFromString(this, File.ReadAllText(paramFilename));
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string FormContents = WinFormStringCnv.ToString(this);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "TEXT|*.txt";
+
+            if (false && sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                File.WriteAllText(sfd.FileName, FormContents);
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                File.WriteAllText(paramFilename, FormContents);
+            }
+
+        }
+
+        private void initPortNameItems()
+        {
+            comboBox_PortName.Items.Clear();
+
+            try
+            {
+                string[] portNames = SerialPort.GetPortNames();
+
+                foreach (string port in portNames)
+                {
+                    comboBox_PortName.Items.Add(port);
+                }
+
+                if (comboBox_PortName.Items.Count > 0)
+                {
+                    comboBox_PortName.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message,
+                                   "port Listup failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            if (button_Connect.Text == "Connect")
+            {
+                try
+                {
+                    string portName = comboBox_PortName.SelectedItem?.ToString();
+                    int baudRate = int.Parse(comboBox_BoadRate.SelectedItem?.ToString() ?? "9600");
+                    int dataBits = int.Parse(comboBox_DataBit.SelectedItem?.ToString() ?? "8");
+                    StopBits stopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBox_StopBit.SelectedItem?.ToString() ?? "One");
+                    Parity parity = (Parity)Enum.Parse(typeof(Parity), comboBox_Parity.SelectedItem?.ToString() ?? "None");
+
+                    serialPort1.PortName = portName;
+                    serialPort1.BaudRate = baudRate;
+                    serialPort1.DataBits = dataBits;
+                    serialPort1.StopBits = stopBits;
+                    serialPort1.Parity = parity;
+                    serialPort1.ReadTimeout = 1000;
+                    serialPort1.WriteTimeout = 1000;
+
+                    if (!serialPort1.IsOpen)
+                    {
+                        serialPort1.Open();
+                    }
+
+                    button_Connect.Text = "DisConnect";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error " + ex.Message,
+                                    "Connection failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    if (serialPort1 != null && serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                        button_Connect.Text = "Connect";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error " + ex.Message,
+                                    "port close failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+        }
+
+        private ushort calculateCRC(byte[] data)
+        {
+            ushort crc = 0xFFFF;
+
+            foreach (byte b in data)
+            {
+                crc ^= b;
+                for (int i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x0001) != 0)
+                        crc = (ushort)((crc >> 1) ^ 0xA001);
+                    else
+                        crc >>= 1;
+                }
+            }
+
+            return crc;
+        }
+
+
+        public ushort[] readRegister(int serverAddress, int startRegisterAddress, int readRegisterCount)
+        {
+            if (serialPort1 == null || !serialPort1.IsOpen)
+            {
+                throw new InvalidOperationException("SerialPort is not open.");
+            }
+
+            // Query: [Address][Function=0x03][StartAddrHi][StartAddrLo][CountHi][CountLo]
+            List<byte> frame = new List<byte>
+            {
+                (byte)serverAddress,
+                0x03,
+                (byte)((startRegisterAddress >> 8) & 0xFF),
+                (byte)(startRegisterAddress & 0xFF),
+                (byte)((readRegisterCount >> 8) & 0xFF),
+                (byte)(readRegisterCount & 0xFF)
+            };
+
+            // CRC-16 [little endian]
+            ushort crc = calculateCRC(frame.ToArray());
+            frame.Add((byte)(crc & 0xFF));
+            frame.Add((byte)((crc >> 8) & 0xFF));
+
+            // send query 
+            serialPort1.DiscardInBuffer();
+            serialPort1.Write(frame.ToArray(), 0, frame.Count);
+
+            // read responce
+            try
+            {
+                // responce: [Address][Function][ByteCount][Data...][CRC_L][CRC_H]
+                byte[] header = new byte[3];
+                serialPort1.Read(header, 0, 3);
+
+                if (header[0] != (byte)serverAddress || header[1] != 0x03)
+                {
+                    throw new InvalidOperationException("responce header is not correct");
+                }
+
+                int byteCount = header[2];
+                byte[] data = new byte[byteCount + 2]; // +2: CRC byte
+                serialPort1.Read(data, 0, data.Length);
+
+                // CRC check
+                List<byte> fullResponse = new List<byte>(header);
+                fullResponse.AddRange(data);
+                ushort receivedCrc = (ushort)(data[data.Length - 2] | (data[data.Length - 1] << 8));
+                ushort calculatedCrc = calculateCRC(fullResponse.GetRange(0, fullResponse.Count - 2).ToArray());
+
+                if (receivedCrc != calculatedCrc)
+                {
+                    throw new InvalidOperationException("CRC error");
+                }
+
+                int registerCount = byteCount / 2;
+                ushort[] registers = new ushort[registerCount];
+                for (int i = 0; i < registerCount; i++)
+                {
+                    registers[i] = (ushort)((data[i * 2] << 8) | data[i * 2 + 1]);
+                }
+
+                return registers;
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException("Modbus timeout");
+            }
+        }
+
+        public void writeRegister(int serverAddress, ushort startRegisterAddress, ushort data)
+        {
+            if (serialPort1 == null || !serialPort1.IsOpen)
+            {
+                throw new InvalidOperationException("SerialPort is not open.");
+            }
+
+            // Query: [Address][Function=0x06][RegisterHi][RegisterLo][DataHi][DataLo]
+            List<byte> frame = new List<byte>
+            {
+                (byte)serverAddress,
+                0x06,
+                (byte)((startRegisterAddress >> 8) & 0xFF),
+                (byte)(startRegisterAddress & 0xFF),
+                (byte)((data >> 8) & 0xFF),
+                (byte)(data & 0xFF)
+            };
+
+            // CRC-16 [little endian]
+            ushort crc = calculateCRC(frame.ToArray());
+            frame.Add((byte)(crc & 0xFF)); 
+            frame.Add((byte)((crc >> 8) & 0xFF));
+
+            serialPort1.DiscardInBuffer();
+            serialPort1.Write(frame.ToArray(), 0, frame.Count);
+
+            // read responce
+            byte[] response = new byte[8];
+            try
+            {
+                serialPort1.Read(response, 0, response.Length);
+
+                // check responce
+                if (response[0] != (byte)serverAddress || response[1] != 0x06)
+                {
+                    throw new InvalidOperationException("Invalid response header.");
+                }
+
+                ushort receivedCrc = (ushort)(response[6] | (response[7] << 8));
+
+                byte[] crcTarget = new byte[6];
+                Array.Copy(response, 0, crcTarget, 0, 6);
+
+                ushort calculatedCrc = calculateCRC(crcTarget);
+
+                if (receivedCrc != calculatedCrc)
+                {
+                    throw new InvalidOperationException("CRC error: response is corrupted.");
+                }
+
+                Console.WriteLine("Register write successful.");
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException("Modbus response timeout.");
+            }
+        }
+
+
+
+        public void writeRegister(int serverAddress, ushort startRegisterAddress, List<ushort> data)
+        {
+            if (serialPort1 == null || !serialPort1.IsOpen)
+            {
+                throw new InvalidOperationException("SerialPort is not open.");
+            }
+
+            int registerCount = data.Count;
+            if (registerCount == 0)
+            {
+                throw new ArgumentException("Data list is empty.");
+            }
+
+            int byteCount = registerCount * 2;
+
+            // Query: [Address][Function=0x10][StartAddrHi][StartAddrLo][CountHi][CountLo][ByteCount][Data...]
+            List<byte> frame = new List<byte>
+            {
+                (byte)serverAddress,
+                0x10,
+                (byte)((startRegisterAddress >> 8) & 0xFF),
+                (byte)(startRegisterAddress & 0xFF),
+                (byte)((registerCount >> 8) & 0xFF),
+                (byte)(registerCount & 0xFF),
+                (byte)byteCount
+            };
+        
+            foreach (ushort value in data)
+            {
+                frame.Add((byte)((value >> 8) & 0xFF));
+                frame.Add((byte)(value & 0xFF));
+            }
+
+            // CRC-16 [little endian]
+            ushort crc = calculateCRC(frame.ToArray());
+            frame.Add((byte)(crc & 0xFF));
+            frame.Add((byte)((crc >> 8) & 0xFF));
+
+            serialPort1.DiscardInBuffer();
+            serialPort1.Write(frame.ToArray(), 0, frame.Count);
+
+            // read responce（ [Address][Function][StartAddrHi][StartAddrLo][CountHi][CountLo][CRC_L][CRC_H]）
+            byte[] response = new byte[8];
+            try
+            {
+                serialPort1.Read(response, 0, response.Length);
+
+                if (response[0] != (byte)serverAddress || response[1] != 0x10)
+                {
+                    throw new InvalidOperationException("Invalid response header.");
+                }
+
+                byte[] crcTarget = new byte[6];
+                Array.Copy(response, 0, crcTarget, 0, 6);
+                ushort receivedCrc = (ushort)(response[6] | (response[7] << 8));
+                ushort calculatedCrc = calculateCRC(crcTarget);
+
+                if (receivedCrc != calculatedCrc)
+                {
+                    throw new InvalidOperationException("CRC error: response is corrupted.");
+                }
+
+                Console.WriteLine("Multiple register write successful.");
+            }
+            catch (TimeoutException)
+            {
+                throw new TimeoutException("Modbus response timeout.");
+            }
+        }
+
+    }
+}
